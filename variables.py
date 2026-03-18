78e791074f8b8daffdc85776b2e40018277653ad
Imrin/ <Imrin.i.dhillon@gobindsarvar.ca>
2026-03-18 12:22:38 -0700

    variable

---
+++ variables.py
@@ -0,0 +1,7 @@
+name = "Imrin Dhillon"
+age = 14
+city = "Surrey"
+school = "Gobind Sarvar"
+print(type(age))
+print(name,age, city,school)
+print(f"\nMy name is {name}, I am {age} years old, born in {city}, and I go to {school}")
